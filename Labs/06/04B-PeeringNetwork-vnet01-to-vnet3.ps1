b4fb2831b9adccd327f760455a322dfa11de7354
Frank <frank.earnhardt@outlook.com>
2022-06-25 11:35:22 -0700

    2022_06_25MOD06-C

---
+++ Labs/06/04B-PeeringNetwork-vnet01-to-vnet3.ps1
@@ -0,0 +1,17 @@
+$mod='az104-06-'
+$rgname = $mod + 'rg1'
+
+$srcnet='vnet01'
+$dscnet='vnet3'
+$name0=$mod + $srcnet
+$name1=$mod + $dscnet
+
+$tag1=$name0 + '_to_' + $name1
+$tag2=$name1 + '_to_' + $name0
+$vnet0 = Get-AzVirtualNetwork -Name $name0 -ResourceGroupName $rgname
+
+$vnet1 = Get-AzVirtualNetwork -Name $name1 -ResourceGroupName $rgname
+
+Add-AzVirtualNetworkPeering -Name $tag1 -VirtualNetwork $vnet0 -RemoteVirtualNetworkId $vnet1.Id
+
+Add-AzVirtualNetworkPeering -Name $tag2 -VirtualNetwork $vnet1 -RemoteVirtualNetworkId $vnet0.Id
